1b4351379a438c73a6d3432cd050def80a788077
user <user@192.168.0.5>
2017-08-29 16:30:50 -0300

    Limpieza de métodos no efectivos.

--- Gimnasio/src/com/utn/entidades/Entrenador.java
+++ Gimnasio/src/com/utn/entidades/Entrenador.java
@@ -3,21 +3,11 @@ package com.utn.entidades;
 public class Entrenador extends Persona {
 
 	String categoria;
-	static int indiceBoxeador = 0;
-	static int indiceEntrenador = 0;
-	int indiceBoxeadorParcial = 0;
-	static Boxeador[] boxeadores;
-	static Entrenador[] entrenadores;
 
 		public Entrenador(String nombre, double peso) {
 			super(nombre, peso);
 		}
-	
-		public static void initialize() {
-			boxeadores = new Boxeador[20];
-			entrenadores = new Entrenador[4];
-		}
-	
+
 		public String getCategoria() {
 			return categoria;
 		}
